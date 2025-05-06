a08c6fce16f243fcce7f07d7c0cc22f3cf667d39
Xavi Artigas <xavierartigas@yahoo.es>
2025-05-06 22:28:13 +0200

    Monitoring wrapper

---
+++ xymdoom.py
@@ -0,0 +1,48 @@
+import time
+import os
+import subprocess
+import keyboard
+
+def process_line(line):
+    if line == "Coin collected":
+        print("Detected coin")
+        keyboard.press(' ')
+        time.sleep(0.1)
+        keyboard.release(' ')
+
+def monitor_log_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            # Move to the end of the file
+            file.seek(0, 2)
+            while process.poll() is None:
+                line = file.readline()
+                if line:
+                    process_line(line.strip())
+                else:
+                    time.sleep(0.5)  # Wait before checking for new lines
+            print("Doom exited")
+    except KeyboardInterrupt:
+        print("\nMonitoring stopped.")
+
+if __name__ == "__main__":
+    log_file_path = "log.txt"
+    # Delete previous log
+    print("Deleting old log")
+    if os.path.exists(log_file_path):
+        os.remove(log_file_path)
+    # Launch a new process in a portable way
+    print("Launching Doom")
+    process = subprocess.Popen([
+        "../gzdoom.exe",
+        "-iwad", "../../DOOM 2/doom2/DOOM2.WAD",
+        "-file", "xymres/", "wads/xymdoom.wad",
+        "+map", "MAP01"],
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # Wait for the log file to be created
+    print("Waiting for log file")
+    while not os.path.exists(log_file_path):
+        time.sleep(1)
+    # Start monitoring the log file
+    print("Monitoring")
+    monitor_log_file(log_file_path)
